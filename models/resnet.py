a823465a32a2e533121d6a4581fb7888300d2d57
krobz <nuaazh@outlook.com>
2020-10-19 17:25:22 +0800

    v1.5

--- models/resnet.py
+++ models/resnet.py
@@ -217,88 +217,111 @@ def init_pretrained_weights(model, model_url):
     print("Initialized model with pretrained weights from {}".format(model_url))
 
 ### change ###
+# class FocalLoss(nn.Module):
+#     """
+#         This criterion is a implemenation of Focal Loss, which is proposed in 
+#         Focal Loss for Dense Object Detection.
+
+#             Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
+
+#         The losses are averaged across observations for each minibatch.
+
+#         Args:
+#             alpha(1D Tensor, Variable) : the scalar factor for this criterion
+#             gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5), 
+#                                    putting more focus on hard, misclassiﬁed examples
+#             size_average(bool): By default, the losses are averaged over observations for each minibatch.
+#                                 However, if the field size_average is set to False, the losses are
+#                                 instead summed for each minibatch.
+
+
+#     """
+#     def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+#         super(FocalLoss, self).__init__()
+#         if alpha is None:
+#             self.alpha = Variable(torch.ones(class_num, 1))
+#         else:
+#             if isinstance(alpha, Variable):
+#                 self.alpha = alpha
+#             else:
+#                 self.alpha = Variable(alpha)
+#         self.gamma = gamma
+#         self.class_num = class_num
+#         self.size_average = size_average
+
+#     def forward(self, inputs, targets):
+#         N = inputs.size(0)
+#         C = inputs.size(1)
+#         P = F.softmax(inputs)
+
+#         class_mask = inputs.data.new(N, C).fill_(0)
+#         class_mask = Variable(class_mask)
+#         ids = targets.view(-1, 1)
+#         class_mask.scatter_(1, ids.data, 1.)
+#         #print(class_mask)
+
+
+#         if inputs.is_cuda and not self.alpha.is_cuda:
+#             self.alpha = self.alpha.cuda()
+#         alpha = self.alpha[ids.data.view(-1)]
+
+#         probs = (P*class_mask).sum(1).view(-1,1)
+
+#         log_p = probs.log()
+#         #print('probs size= {}'.format(probs.size()))
+#         #print(probs)
+
+#         batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
+#         #print('-----bacth_loss------')
+#         #print(batch_loss)
+
+
+#         if self.size_average:
+#             loss = batch_loss.mean()
+#         else:
+#             loss = batch_loss.sum()
+#         return loss
+
+#focal in kaggle
 class FocalLoss(nn.Module):
-    """
-        This criterion is a implemenation of Focal Loss, which is proposed in 
-        Focal Loss for Dense Object Detection.
-
-            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
-
-        The losses are averaged across observations for each minibatch.
-
-        Args:
-            alpha(1D Tensor, Variable) : the scalar factor for this criterion
-            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5), 
-                                   putting more focus on hard, misclassiﬁed examples
-            size_average(bool): By default, the losses are averaged over observations for each minibatch.
-                                However, if the field size_average is set to False, the losses are
-                                instead summed for each minibatch.
-
-
-    """
-    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True):
         super(FocalLoss, self).__init__()
-        if alpha is None:
-            self.alpha = Variable(torch.ones(class_num, 1))
-        else:
-            if isinstance(alpha, Variable):
-                self.alpha = alpha
-            else:
-                self.alpha = Variable(alpha)
+        self.alpha = alpha
         self.gamma = gamma
-        self.class_num = class_num
-        self.size_average = size_average
+        self.logits = logits
+        self.reduce = reduce
 
     def forward(self, inputs, targets):
-        N = inputs.size(0)
-        C = inputs.size(1)
-        P = F.softmax(inputs)
-
-        class_mask = inputs.data.new(N, C).fill_(0)
-        class_mask = Variable(class_mask)
-        ids = targets.view(-1, 1)
-        class_mask.scatter_(1, ids.data, 1.)
-        #print(class_mask)
-
-
-        if inputs.is_cuda and not self.alpha.is_cuda:
-            self.alpha = self.alpha.cuda()
-        alpha = self.alpha[ids.data.view(-1)]
-
-        probs = (P*class_mask).sum(1).view(-1,1)
-
-        log_p = probs.log()
-        #print('probs size= {}'.format(probs.size()))
-        #print(probs)
-
-        batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
-        #print('-----bacth_loss------')
-        #print(batch_loss)
-
-
-        if self.size_average:
-            loss = batch_loss.mean()
+        if self.logits:
+            BCE_loss = F.binary_cross_entropy_with_logits(inputs, targets, reduce=False)
         else:
-            loss = batch_loss.sum()
-        return loss
+            BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
 
-class BCEFocalLoss(torch.nn.Module):
-  """
-  binary classfication Focalloss, fixed alpha
-  """
-  def __init__(self, gamma=2, alpha=0.25, reduction='elementwise_mean'):
-    super().__init__()
-    self.gamma = gamma
-    self.alpha = alpha
-    self.reduction = reduction
+        if self.reduce:
+            return torch.mean(F_loss)
+        else:
+            return F_loss
+
+
+# class BCEFocalLoss(torch.nn.Module):
+#   """
+#   binary classfication Focalloss, fixed alpha
+#   """
+#   def __init__(self, gamma=2, alpha=0.25, reduction='elementwise_mean'):
+#     super().__init__()
+#     self.gamma = gamma
+#     self.alpha = alpha
+#     self.reduction = reduction
   
-  def forward(self, _input, target):
-    pt = torch.sigmoid(_input)
-    alpha = self.alpha
-    loss = - alpha * (1 - pt) ** self.gamma * target * torch.log(pt) - \
-        (1 - alpha) * pt ** self.gamma * (1 - target) * torch.log(1 - pt)
-    if self.reduction == 'elementwise_mean':
-      loss = torch.mean(loss)
-    elif self.reduction == 'sum':
-      loss = torch.sum(loss)
-    return loss
+#   def forward(self, _input, target):
+#     pt = torch.sigmoid(_input)
+#     alpha = self.alpha
+#     loss = - alpha * (1 - pt) ** self.gamma * target * torch.log(pt) - \
+#         (1 - alpha) * pt ** self.gamma * (1 - target) * torch.log(1 - pt)
+#     if self.reduction == 'elementwise_mean':
+#       loss = torch.mean(loss)
+#     elif self.reduction == 'sum':
+#       loss = torch.sum(loss)
+#     return loss
